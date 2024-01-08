05ba97649c60131dc5ebe6789d94ad223fc1fc82
Zakerxa <zakerxa@gmail.com>
2024-01-08 14:57:06 +0630

    fix contactus address

--- resources/js/store/modules/contactus.js
+++ resources/js/store/modules/contactus.js
@@ -1,32 +1,28 @@
 export default {
     state() {
         return {
-            phone: '+959962330407'
-        }
-    },
-    getters: {
-        phoneNumber(state) {
-            return state.phone
+            phone: '+959777637858'
         }
     },
     actions: {
         openInFacebookApp({ state, commit, rootState }) {
-            var fbPageURL = "https://www.facebook.com/profile.php?id=61554458446614";
+            var fbPageID = 61554458446614;
+            var fbAppURL = "fb://page/" + fbPageID;
             // Check if the user is on a mobile device
             if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-                // Attempt to open in the Facebook app
-                window.location.href = "intent://page/" + fbPageURL.substr(fbPageURL.lastIndexOf('/') + 1) + "#Intent;package=com.facebook.katana;scheme=https;end";
-            } else {
-                // If not on a mobile device, open in the default browser
-                window.open(fbPageURL, "_blank");
-            }
+                try {
+                    window.location.href = fbAppURL;
+                } catch (error) {
+                    window.location.href = "intent://page/" + fbPageURL.substr(fbPageURL.lastIndexOf('/') + 1) + "#Intent;package=com.facebook.katana;scheme=https;end";
+                }
+            } else window.location.href = "https://www.facebook.com/profile.php?id=" + fbPageID;
+
         },
         openInTelegram({ state, commit, rootState }) {
-            window.location.href = "https://t.me/share/url?url=tel:" + state.phone;
+            window.location.href = "https://t.me/" + encodeURIComponent(state.phone);
         },
         openInWhatsApp({ state, commit, rootState }) {
-            window.location.href = "https://wa.me/" + state.phone;
-            // window.open('https://api.whatsapp.com/send?phone=+959962330407', '_blank');
+            window.open(`https://api.whatsapp.com/send?phone=${state.phone}`, '_blank');
         },
         openInPhone({ state, commit, rootState }) {
             window.location.href = `tel:${state.phone}`;
