67fc2916c5d2b5100c86aaa9c13f9e8a41b14a5c
Zakerxa <zakerxa@gmail.com>
2024-01-26 04:12:24 +0630

    updated gender

--- resources/js/components/Explore/MultiFilter.vue
+++ resources/js/components/Explore/MultiFilter.vue
@@ -368,6 +368,14 @@ export default {
                 ]
             }
             ],
+            optionsGender: [{
+                id: 1,
+                name: 'Female'
+            },
+            {
+                id: 2,
+                name: 'Male'
+            }],
             name: '',
             gender: '',
             region: [],
